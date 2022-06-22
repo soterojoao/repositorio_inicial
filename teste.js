ab0b565a730c39b0976020e74a585a7fb6bf7d2a
soterojoao <joaosotero12@gmail.com>
2022-06-22 15:43:47 -0300

    jogo da memoria

--- teste.js
+++ teste.js
@@ -1,5 +1,22 @@
-var a=89;
-var b=11;
-var total=a+b;
-console.log(total);
-console.log("Hello World!!");
+var n1 = prompt("Digite um numero");
+var n2 = prompt("Digite um numero");
+function soma (a,b){
+    return Number(a)+Number(b);
+}
+var n3 = soma(n1,n2);
+if(n3<10){
+    var a1="menor que 10";
+} else{
+    var a1="maior que 10";
+}
+if(n3<20){
+    var a2="menor que 20";
+}else{
+    var a2="maior que 20";
+}
+if(n1==n2){
+    var a3="Sao iguais";
+}else{
+    var a3="Sao diferentes";
+}
+console.log(a3, a2, a1);
